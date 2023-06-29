3ba3e1fdf97a0a3f99183ad7f0c2dfb0ce77ae8a
Roberto640Alvarado <00176620@uca.edu.sv>
2023-06-29 17:56:10 -0600

    Deploy8

--- src/main/java/com/grupo9/blueTicket/repositories/UserRepository.java
+++ src/main/java/com/grupo9/blueTicket/repositories/UserRepository.java
@@ -3,12 +3,13 @@ package com.grupo9.blueTicket.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.grupo9.blueTicket.models.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID>{
+public interface UserRepository extends ListCrudRepository<User, UUID>{
 	public User findOneByUsernameOrEmail(String username, String email);
 
 }
