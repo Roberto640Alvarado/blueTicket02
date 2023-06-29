158e83e28b08ec7a326f8cf645f202a84d21ebf5
Roberto640Alvarado <00176620@uca.edu.sv>
2023-06-29 17:37:27 -0600

    Deploy6

--- src/main/java/com/grupo9/blueTicket/BlueTicketApplication.java
+++ src/main/java/com/grupo9/blueTicket/BlueTicketApplication.java
@@ -5,10 +5,12 @@ import java.security.SecureRandom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BlueTicketApplication {
 	
 	@Bean
