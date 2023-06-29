00d4392ecdf880f2cf4b62806dd584e1f8b8b296
Roberto640Alvarado <00176620@uca.edu.sv>
2023-06-29 17:59:19 -0600

    Deploy9

--- src/main/java/com/grupo9/blueTicket/repositories/TokenRepository.java
+++ src/main/java/com/grupo9/blueTicket/repositories/TokenRepository.java
@@ -3,12 +3,13 @@ package com.grupo9.blueTicket.repositories;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 
 import com.grupo9.blueTicket.models.entities.Token;
 import com.grupo9.blueTicket.models.entities.User;
 
-public interface TokenRepository extends ListCrudRepository<Token, UUID>{
+public interface TokenRepository extends JpaRepository<Token, UUID>{
 	List<Token> findByUserAndActive(User user, Boolean active);
 
 }
--- src/main/java/com/grupo9/blueTicket/repositories/UserRepository.java
+++ src/main/java/com/grupo9/blueTicket/repositories/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.grupo9.blueTicket.models.entities.User;
 
 @Repository
-public interface UserRepository extends ListCrudRepository<User, UUID>{
+public interface UserRepository extends JpaRepository<User, UUID>{
 	public User findOneByUsernameOrEmail(String username, String email);
 
 }
